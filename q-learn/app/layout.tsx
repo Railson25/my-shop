274b852fcc498ed10dfe9d65f521f133378a47c2
Railson25 <railsonsantiago123@gmail.com>
2023-09-04 13:39:45 -0300

    <feat>: Navbar component

--- q-learn/app/layout.tsx
+++ q-learn/app/layout.tsx
@@ -1,6 +1,8 @@
+import { Navbar } from '@/components/navbar'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { ClientOnly } from '@/components/client-only'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +18,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ClientOnly>
+          <Navbar />
+        </ClientOnly>
+        {children}
+        </body>
     </html>
   )
 }

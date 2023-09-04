e5b281de9b931a0164a75a760c7e8b8763f2cfd2
Railson25 <railsonsantiago123@gmail.com>
2023-09-04 18:43:26 -0300

    <fix>: Button properties

--- q-learn/components/button.tsx
+++ q-learn/components/button.tsx
@@ -1,20 +1,20 @@
 "use client"
 
-interface ButtonProps{
+import {twMerge} from 'tailwind-merge'
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement>{
     label: string
     outline?: boolean 
-    onClick: () => void
 }
 
-export const Button = ({label, outline, onClick}: ButtonProps) => {
+export const Button = ({label, outline, onClick, className}: ButtonProps) => {
     return(
         <button 
-            className={`
-                w-[153px] h-14 p-3 border-[2px] border-solid border-purple-900/75 
-                rounded-s-2xl rounded-br-2xl text-2xl font-bold 
-                ${outline ? 'bg-white': ' bg-purple-900'}
-                ${outline ? 'text-neutral-500': 'text-white'}
-            `}
+            className={twMerge(`w-[153px] h-14 p-3 border-[2px] border-solid border-purple-900/75 rounded-s-2xl rounded-br-2xl text-2xl font-bold 
+            ${outline ? 'bg-white': ' bg-purple-900'}
+            ${outline ? 'text-neutral-500': 'text-white'}
+            `, 
+            className)}
         >
             {label}
         </button>

9ad4a8b02a776dec65cfb2ca8b8441f9a4a94e7d
Railson25 <railsonsantiago123@gmail.com>
2023-09-05 17:35:28 -0300

    <feat>: testimonial component

--- q-learn/app/page.tsx
+++ q-learn/app/page.tsx
@@ -3,6 +3,7 @@ import { ClientOnly } from "@/components/client-only";
 import { Course } from "@/components/course";
 import { Header } from "@/components/header";
 import { Teachers } from "@/components/teachers";
+import { Testimonials } from "@/components/testimonials";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <Categories />
       <Teachers />
       <Course />
+      <Testimonials />
     </ClientOnly>
     </>
   )

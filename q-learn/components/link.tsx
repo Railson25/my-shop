b32eb50d6e1cf8c0ecb9c12be98972ec268b4592
Railson25 <railsonsantiago123@gmail.com>
2023-09-11 13:45:58 -0300

    <style>: Footer responsive

--- q-learn/components/link.tsx
+++ q-learn/components/link.tsx
@@ -12,7 +12,7 @@ interface LinksProps{
 
 export const Links = ({className, contact, service, title, page}: LinksProps) => {
     return(
-        <div className={twMerge("grow flex flex-col items-start mb-[168px]", className)}>
+        <div className={twMerge("grow flex flex-col items-start mb-[90px] md:mb-[168px] ", className)}>
             <h1 className="text-white text-[30px] leading-[37px] font-bold pb-3">
                 {title}
             </h1>

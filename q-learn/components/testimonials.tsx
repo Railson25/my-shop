da0b8be09d405653f2441b07e5b3905f6c6a6551
Railson25 <railsonsantiago123@gmail.com>
2023-09-13 14:43:24 -0300

    <style>: Testimonials responsive

--- q-learn/components/testimonials.tsx
+++ q-learn/components/testimonials.tsx
@@ -8,7 +8,7 @@ export const Testimonials = () => {
                 title="What our students say"
                 description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been th "
             />
-            <div className="flex gap-x-10 mt-[46px]">
+            <div className="gap-x-10 mt-[46px] grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 ">
                 <TestimonialsCard 
                     src="/images/testimonial-image-one.jpg"
                     description="Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.  "

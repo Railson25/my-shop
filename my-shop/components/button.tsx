710f5e4bc91affb16afaa3aeb9be60ca1bc0b9d6
Railson25 <railsonsantiago123@gmail.com>
2023-09-19 22:47:33 -0300

    <fix>: button options

--- my-shop/components/button.tsx
+++ my-shop/components/button.tsx
@@ -5,12 +5,25 @@ import {twMerge} from "tailwind-merge"
 interface ButtonProps {
     className?: string
     label: string
+    banner?: boolean
 }
 
-export const Button = ({label, className}: ButtonProps) => {
+export const Button = ({label, className, banner}: ButtonProps) => {
     return(
         <button 
-            className={twMerge("text-[14px] font-semibold py-[15px] px-[30px] text-black bg-white rounded cursor-pointer border-none outline-none transition", className)}
+            className={twMerge(`font-semibold cursor-pointer outline-none transition
+                ${banner ? 'text-[13px]' : 'text-[14px]'}
+                ${banner ? 'py-[11px]' : 'py-[15px]'}
+                ${banner ? 'px-[18px]' : 'px-[30px]'}
+                ${banner ? 'text-white' : 'text-black'}
+                ${banner ? 'border' : 'border-none'}
+                ${banner ? 'rounded-none' : 'rounded'}
+                ${banner ? 'bg-transparent' : 'bg-white'}
+                ${banner ? 'group-hover:bg-[#088178]' : ''}
+                ${banner ? 'group-hover:border-[#088178]' : ''}
+                ${banner ? 'border-solid' : 'border-none'}
+                ${banner ? 'border-white' : ''}
+            `, className)}
         >
             {label}
         </button>

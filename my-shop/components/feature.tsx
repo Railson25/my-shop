48886aa5c75e20606c94e3179ef1cec8e2058eb5
Railson25 <railsonsantiago123@gmail.com>
2023-09-21 13:59:27 -0300

    <style>: responsive Feature

--- my-shop/components/feature.tsx
+++ my-shop/components/feature.tsx
@@ -1,40 +1,36 @@
-'use client'
+"use client";
 
-import { FeatureCard } from "./feature-card"
+import { FeatureCard } from "./feature-card";
 
 export const Feature = () => {
-    return(
-        <div className="py-10 px-20 bg-white flex items-center justify-between flex-wrap">
-            <FeatureCard 
-                src='/images/f1.png'
-                label="Free Shipping"
-            />
-            <FeatureCard 
-                className="bg-[#cdebbc]"
-                src='/images/f2.png'
-                label="Online Order"
-            />
-            <FeatureCard 
-                className="bg-[#d1e8f2]"
-                src='/images/f3.png'
-                label="Save Money"
-            />
-            <FeatureCard 
-                className="bg-[#cdd4f8]"
-                src='/images/f4.png'
-                label="Promotions"
-            />
-            <FeatureCard 
-                className="bg-[#f6dbf6]"
-                src='/images/f5.png'
-                label="Happy Sell"
-            />
-            <FeatureCard 
-                className="bg-[#fff2e5]"
-                src='/images/f6.png'
-                label="F24/7 Support"
-            />
-            
-        </div>
-    )
-}
+  return (
+    <div className="py-10 px-20 bg-white flex items-center justify-between flex-wrap max-md:justify-center max-md:gap-x-[15px] max-md:px-10">
+      <FeatureCard src="/images/f1.png" label="Free Shipping" />
+      <FeatureCard
+        className="bg-[#cdebbc]"
+        src="/images/f2.png"
+        label="Online Order"
+      />
+      <FeatureCard
+        className="bg-[#d1e8f2]"
+        src="/images/f3.png"
+        label="Save Money"
+      />
+      <FeatureCard
+        className="bg-[#cdd4f8]"
+        src="/images/f4.png"
+        label="Promotions"
+      />
+      <FeatureCard
+        className="bg-[#f6dbf6]"
+        src="/images/f5.png"
+        label="Happy Sell"
+      />
+      <FeatureCard
+        className="bg-[#fff2e5]"
+        src="/images/f6.png"
+        label="F24/7 Support"
+      />
+    </div>
+  );
+};

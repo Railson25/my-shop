bd15cc2ddeb08dd62149e1fd7bff96d4b9ef149f
Railson25 <railsonsantiago123@gmail.com>
2023-09-21 14:25:54 -0300

    <style>: responsive  information

--- my-shop/components/information.tsx
+++ my-shop/components/information.tsx
@@ -1,16 +1,22 @@
-"use client"
+"use client";
 
-import {twMerge} from 'tailwind-merge'
+import { twMerge } from "tailwind-merge";
 
 interface InformationProps {
-    children: React.ReactNode
-    className?: string
+  children: React.ReactNode;
+  className?: string;
 }
 
-export const Information = ({children, className}: InformationProps) => {
-    return(
-        <div className={twMerge(`py-10 px-20 flex  justify-between flex-wrap bg-white`, className)}>
-           {children}
-        </div>
-    )
-}
+export const Information = ({ children, className }: InformationProps) => {
+  return (
+    <div
+      className={twMerge(
+        `py-10 px-20 flex  justify-between flex-wrap bg-white 
+        `,
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+};

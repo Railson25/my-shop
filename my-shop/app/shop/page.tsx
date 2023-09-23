ef25474115f646f6c75c6824b1e1d66748407ef0
Railson25 <railsonsantiago123@gmail.com>
2023-09-23 00:58:25 -0300

    <feat>: shop page

---
+++ my-shop/app/shop/page.tsx
@@ -0,0 +1,17 @@
+import { Header } from "@/components/header";
+
+const Shop = () => {
+  return (
+    <div>
+      <Header
+        subtitle="#stayhome"
+        paragraph="Save more with coupon & up to 70% off"
+        h2Style="font-bold text-white"
+        pStyle="text-white"
+        className="bg-[url('/images/banner/b1.jpg')]  h-[40vh] items-center p-[14px] "
+      />
+    </div>
+  );
+};
+
+export default Shop;

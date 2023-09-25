06e3648a5f27147895850d2e460c26c490846bd8
Railson25 <railsonsantiago123@gmail.com>
2023-09-25 15:36:01 -0300

    <style>: button

--- my-shop/components/button.tsx
+++ my-shop/components/button.tsx
@@ -1,31 +1,67 @@
-"use client"
+"use client";
 
-import {twMerge} from "tailwind-merge"
+import { IconType } from "react-icons";
+import { twMerge } from "tailwind-merge";
 
 interface ButtonProps {
-    className?: string
-    label: string
-    banner?: boolean
+  className?: string;
+  label?: string;
+  banner?: boolean;
+  pagination?: boolean;
+  icon?: IconType;
+  onClick?: () => void;
+  children?: React.ReactNode;
+  outline?: boolean;
 }
 
-export const Button = ({label, className, banner}: ButtonProps) => {
-    return(
-        <button 
-            className={twMerge(`font-semibold cursor-pointer outline-none transition
-                ${banner ? 'text-[13px]' : 'text-[14px]'}
-                ${banner ? 'py-[11px]' : 'py-[15px]'}
-                ${banner ? 'px-[18px]' : 'px-[30px]'}
-                ${banner ? 'text-white' : 'text-black'}
-                ${banner ? 'border' : 'border-none'}
-                ${banner ? 'rounded-none' : 'rounded'}
-                ${banner ? 'bg-transparent' : 'bg-white'}
-                ${banner ? 'group-hover:bg-[#088178]' : ''}
-                ${banner ? 'group-hover:border-[#088178]' : ''}
-                ${banner ? 'border-solid' : 'border-none'}
-                ${banner ? 'border-white' : ''}
-            `, className)}
-        >
-            {label}
-        </button>
-    )
-}
+export const Button = ({
+  label,
+  className,
+  banner,
+  icon: Icon,
+  onClick,
+  pagination,
+  children,
+  outline,
+}: ButtonProps) => {
+  return (
+    <button
+      className={twMerge(
+        `font-semibold cursor-pointer outline-none transition
+                ${banner ? "text-[13px]" : "text-[14px]"}
+                ${banner ? "py-[11px]" : "py-[15px]"}
+                ${banner ? "px-[18px]" : "px-[30px]"}
+                ${banner ? "text-white" : "text-black"}
+                ${banner ? "border" : "border-none"}
+                ${banner ? "rounded-none" : "rounded"}
+                ${banner ? "bg-transparent" : "bg-white"}
+                ${banner ? "group-hover:bg-[#088178]" : ""}
+                ${banner ? "group-hover:border-[#088178]" : ""}
+                ${banner ? "border-solid" : "border-none"}
+                ${banner ? "border-white" : ""}
+                ${pagination ? "bg-[#088175]" : ""}
+                ${pagination ? "py-[15px]" : ""}
+                ${pagination ? "px-[20px]" : ""}
+                ${pagination ? "text-white" : ""}
+                ${pagination ? "rounded" : ""}
+                ${pagination ? "font-semibold" : ""}
+                ${
+                  pagination
+                    ? "hover:bg-white  border border-solid border-[#088175]"
+                    : ""
+                }
+                ${pagination ? "hover:text-neutral-700" : ""}
+                ${outline ? "bg-white" : ""}
+                ${outline ? "text-[#088175]" : ""}
+            `,
+        className
+      )}
+      onClick={onClick}
+    >
+      {label}
+
+      {children}
+      {Icon && <Icon size={22} />}
+    </button>
+  );
+};

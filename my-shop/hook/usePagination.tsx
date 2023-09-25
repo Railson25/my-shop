563ac7d777becf9b78b0aedb54e45b9fe6fc02b7
Railson25 <railsonsantiago123@gmail.com>
2023-09-25 15:35:27 -0300

    <feat>: Pagination hook

---
+++ my-shop/hook/usePagination.tsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+export function usePagination<T>(data: T[], itemPerPage: number) {
+  const [currentPage, setCurrentPage] = useState(1);
+  const maxPage = Math.ceil(data.length / itemPerPage);
+
+  function currentData() {
+    const begin = (currentPage - 1) * itemPerPage;
+    const end = begin + itemPerPage;
+    return data.slice(begin, end);
+  }
+
+  function nextPage() {
+    setCurrentPage((prev) => Math.min(prev + 1, maxPage));
+  }
+
+  function backPage() {
+    setCurrentPage((prev) => Math.max(prev - 1, 1));
+  }
+
+  function goToPage(pageNumber: number) {
+    setCurrentPage(Math.min(Math.max(pageNumber, 1), maxPage));
+  }
+
+  function goToInitialPage() {
+    setCurrentPage(1);
+  }
+  function goToLastPage() {
+    setCurrentPage(maxPage);
+  }
+
+  return {
+    currentPage,
+    setCurrentPage,
+    maxPage,
+    currentData,
+    nextPage,
+    backPage,
+    goToPage,
+    goToInitialPage,
+    goToLastPage,
+  };
+}

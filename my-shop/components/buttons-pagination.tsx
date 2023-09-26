1d8b80e151c40658e04120fd7132c6e0bd6ef13e
Railson25 <railsonsantiago123@gmail.com>
2023-09-25 23:18:35 -0300

    <fix>: generate pages function

--- my-shop/components/buttons-pagination.tsx
+++ my-shop/components/buttons-pagination.tsx
@@ -23,26 +23,44 @@ export const ButtonsPagination = ({
   goToInitialPage,
   goToLastPage,
 }: ButtonsPaginationProps) => {
+  const generatePages = () => {
+    const pages: number[] = [];
+    const pagesAroundCurrent = 1;
+
+    const startPage = Math.max(2, pageNumber - pagesAroundCurrent);
+    const endPage = Math.min(maxPage - 1, pageNumber + pagesAroundCurrent);
+
+    for (let i = 1; i <= maxPage; i++) {
+      if (i === 1 || i === maxPage || (i >= startPage && i <= endPage)) {
+        pages.push(i);
+      } else if (pages[pages.length - 1] !== -1) {
+        pages.push(-1);
+      }
+    }
+
+    return pages;
+  };
+
+  console.log(pageNumber);
   return (
     <div className="py-10 px-20 flex text-center justify-center gap-x-2 ">
-      {pageNumber !== 1 ? (
-        <Button pagination icon={BsArrowLeft} onClick={onPrev} />
-      ) : (
-        <></>
+      <Button pagination icon={BsArrowLeft} onClick={onPrev} />
+      {generatePages().map((page, index) =>
+        page === -1 ? (
+          <span key={index} className="flex items-end px-5">
+            ...
+          </span>
+        ) : (
+          <Button
+            pagination
+            onClick={() => onPage?.(page)}
+            key={index}
+            outline={page === pageNumber}
+          >
+            {page}
+          </Button>
+        )
       )}
-      <Button pagination onClick={goToInitialPage} outline>
-        {pageNumber}
-      </Button>
-
-      <Button pagination onClick={() => onPage?.(pageNumber)}>
-        {pageNumber !== maxPage ? pageNumber + 1 : pageNumber}
-      </Button>
-
-      <span className="flex items-end px-5">...</span>
-
-      <Button pagination onClick={goToLastPage}>
-        {pageNumber !== maxPage ? pageNumber + 1 : pageNumber}
-      </Button>
 
       <Button pagination icon={BsArrowRight} onClick={onNext} />
     </div>

2994de1bfb1fc7802b3a11b6a7814c11f4146efc
Railson25 <railsonsantiago123@gmail.com>
2023-09-27 14:04:45 -0300

    <feat>: blog data

---
+++ my-shop/mock/block.ts
@@ -0,0 +1,44 @@
+interface BlogItemType {
+    src: string;
+    title: string;
+    description: string;
+    date: string;
+  }
+  
+export const blogItems: BlogItemType[] = [
+{
+    src: "/images/blog/b1.jpg",
+    title: "The Cotton-Jersey Zip-Up Hoodie",
+    description:
+    "Kickstart man braid godard coloring book. Raclette         waistcoat selfies yr wolf chartreuse hexagon irony, godard...",
+    date: "13/01",
+},
+{
+    src: "/images/blog/b2.jpg",
+    title: "How to Style a Quiff",
+    description:
+    "Kickstart man braid godard coloring book. Raclette         waistcoat selfies yr wolf chartreuse hexagon irony, godard...",
+    date: "12/01",
+},
+{
+    src: "/images/blog/b3.jpg",
+    title: "Must-Have Skater Girl Items",
+    description:
+    "Kickstart man braid godard coloring book. Raclette         waistcoat selfies yr wolf chartreuse hexagon irony, godard...",
+    date: "2/03",
+},
+{
+    src: "/images/blog/b4.jpg",
+    title: "Runway-Inspired Trends",
+    description:
+    "Kickstart man braid godard coloring book. Raclette         waistcoat selfies yr wolf chartreuse hexagon irony, godard...",
+    date: "8/03",
+},
+{
+    src: "/images/blog/b6.jpg",
+    title: "AW20 Menswear Trends",
+    description:
+    "Kickstart man braid godard coloring book. Raclette         waistcoat selfies yr wolf chartreuse hexagon irony, godard...",
+    date: "15/04",
+},
+];

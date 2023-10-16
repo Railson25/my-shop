03762743d40a32767773f072865fa48d901f3b4b
Railson25 <railsonsantiago123@gmail.com>
2023-10-16 11:25:44 -0300

    <refactor>: getting and show our categories

--- lms-tutorial/app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ lms-tutorial/app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -6,6 +6,7 @@ import { redirect } from "next/navigation"
 import { TitleForm } from "./_components/title-form"
 import { DescriptionForm } from "./_components/description-form"
 import { ImageForm } from "./_components/image-form"
+import { CategoryForm } from "./_components/category-form"
 
 const CourseIdPage = async ({params}: {params: {courseId: string}}) =>{
     const {userId} = auth()
@@ -19,6 +20,14 @@ const CourseIdPage = async ({params}: {params: {courseId: string}}) =>{
             id: params.courseId
         }
     })
+
+    const  categories = await db.category.findMany({
+        orderBy: {
+            name: 'asc'
+        }
+    })
+
+    console.log(categories)
 
     if(!course){
         return redirect('/')
@@ -71,6 +80,14 @@ const CourseIdPage = async ({params}: {params: {courseId: string}}) =>{
                 <ImageForm 
                     initialData={course}
                     courseId={course.id}
+                />
+                <CategoryForm 
+                    initialData={course}
+                    courseId={course.id}
+                    options={categories.map((category) => ({
+                        label: category.name,
+                        value: category.id
+                    }))}
                 />
             </div>
           </div>

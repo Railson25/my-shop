d12b13aa50ebc50cf4150059f2228c484a6a2e7c
Railson25 <railsonsantiago123@gmail.com>
2023-10-18 13:20:58 -0300

    <feat>: chapter description form component

--- lms-tutorial/app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ lms-tutorial/app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -5,6 +5,7 @@ import { ArrowLeft, LayoutDashboard } from "lucide-react";
 import Link from "next/link";
 import { redirect } from "next/navigation";
 import { ChapterTitleForm } from "./_components/chapter-title-form";
+import { ChapterDescriptionForm } from "./_components/chapter-description-form";
 
 const ChapterIdPage = async ({params}: {params: {courseId: string; chapterId: string}}) => {
 
@@ -76,6 +77,11 @@ const ChapterIdPage = async ({params}: {params: {courseId: string; chapterId: st
                             initialData={chapter}
                             chapterId={params.chapterId}
                             courseId={params.courseId}
+                        />
+                        <ChapterDescriptionForm 
+                            initialData={chapter}
+                            courseId={params.courseId}
+                            chapterId={params.chapterId}
                         />
                     </div>
                 </div>

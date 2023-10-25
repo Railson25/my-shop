51e740b2dd73c2e1518666c2bcb81fbd8e843cb9
Railson25 <railsonsantiago123@gmail.com>
2023-10-25 14:45:28 -0300

    <feat>: onClick function to payment

--- lms-tutorial/app/(course)/courses/[courseId]/chapters/[chapterId]/_components/course-entoll-button.tsx
+++ lms-tutorial/app/(course)/courses/[courseId]/chapters/[chapterId]/_components/course-entoll-button.tsx
@@ -2,6 +2,9 @@
 
 import {Button} from '@/components/ui/button'
 import {formatPrice} from '@/lib/format'
+import axios from 'axios'
+import { useState } from 'react'
+import toast from 'react-hot-toast'
 
 interface CourseEnrollButtonProps{
     price: number
@@ -9,8 +12,27 @@ interface CourseEnrollButtonProps{
 }
 
 export const CourseEnrollButton = ({price, courseId}: CourseEnrollButtonProps) => {
+    const [isLoading, setIsLoading] = useState(false)
+
+    const onClick = async () => {
+        try {
+            setIsLoading(true)
+
+            const response = await axios.post(`/api/courses/${courseId}/checkout`)
+
+            window.location.assign(response.data.url)
+
+        } catch (error) {
+            toast.error('Something went wrong')
+        }finally{
+            setIsLoading(false)
+        }
+    }
+
     return(
         <Button
+            onClick={onClick}
+            disabled={isLoading}
             size='sm'
             className='w-full md:w-auto'
         >

d5ca16f2b35a701f2a05bad7fd164170bf50c70e
Railson25 <railsonsantiago123@gmail.com>
2023-10-26 11:32:45 -0300

    <feat> showing course progress

--- lms-tutorial/components/course-card.tsx
+++ lms-tutorial/components/course-card.tsx
@@ -3,6 +3,7 @@ import Link from "next/link"
 import { IconBadge } from "./icon-badge"
 import { BookOpen } from "lucide-react"
 import { formatPrice } from "@/lib/format"
+import { CourseProgress } from "./course-progress"
 
 interface CourseCardProps{
     id: string
@@ -49,7 +50,11 @@ export const CourseCard = ({category, chaptersLength, id, imageUrl, price, progr
                    {progress !== null ? 
                         (
                             <div>
-                                TODO: Progress components
+                                <CourseProgress 
+                                    value={progress}
+                                    size="sm"
+                                    variant={progress === 100 ? 'success':  'default'}
+                                />
                             </div>
                         ):(
                             <p className="text-md md:text-sm font-medium text-slate-700">

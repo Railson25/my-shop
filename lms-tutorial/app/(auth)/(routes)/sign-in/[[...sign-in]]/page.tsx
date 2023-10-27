ac490d20ae05e515ad8b07ea14618203dfeae581
Railson25 <railsonsantiago123@gmail.com>
2023-10-27 14:04:56 -0300

    <style>: Login and Sign in page

--- lms-tutorial/app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
+++ lms-tutorial/app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,18 @@
 import { SignIn } from "@clerk/nextjs";
+import Image from "next/image";
 
 export default function Page() {
-  return <SignIn />;
+  return (
+    <div className="flex">
+      <Image 
+        alt="Login Image"
+        src='/login.jpg'
+        width={1024}
+        height={720}
+        className="w-[300px] rounded-xl max-md:hidden"
+      />
+      <SignIn />;
+    </div>
+  )
+  
 }
--- lms-tutorial/app/(auth)/layout.tsx
+++ lms-tutorial/app/(auth)/layout.tsx
@@ -1,7 +1,9 @@
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className="w-full h-full flex justify-center items-center">
-      {children}
+    <main className="w-full h-full flex justify-center items-center bg-gradient-to-r from-[#FC7766] via-[#F5DACF] to-[#045F68] ">
+      <div className="">
+        {children}
+      </div>
     </main>
   );
 };

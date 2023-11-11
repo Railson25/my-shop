a476eac460d9b51cd3303f898796fb9cc38f39d3
Railson25 <railsonsantiago123@gmail.com>
2023-11-11 10:44:21 -0300

    <refactor>: folder structure

---
+++ notion-clone/app/(auth)/layout.tsx
@@ -0,0 +1,4 @@
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
+  return <div className="h-full ">{children}</div>;
+};
+export default RootLayout;
---
+++ notion-clone/app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>kgh</h1>
+      <Button>enviar</Button>
+    </div>
+  );
+}

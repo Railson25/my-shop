c5e56dc87484ac6bcd0a7499d5e5772d76b5b09e
Railson25 <railsonsantiago123@gmail.com>
2023-11-14 13:26:28 -0300

    <style>: Documents Page

--- notion-clone/app/(main)/(routes)/documents/page.tsx
+++ notion-clone/app/(main)/(routes)/documents/page.tsx
@@ -1,5 +1,38 @@
+"use client";
+
+import Image from "next/image";
+import { useUser } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+import { PlusCircle } from "lucide-react";
+
 const DocumentsPage = () => {
-  return <h1>wegfw</h1>;
+  const { user } = useUser();
+
+  return (
+    <div className="h-full flex flex-col items-center  justify-center">
+      <Image
+        src="/empty.png"
+        alt="Empty"
+        width="300"
+        height="300"
+        className="dark:hidden"
+      />
+      <Image
+        src="/empty-dark.png"
+        alt="Empty"
+        width="300"
+        height="300"
+        className="hidden dark:block"
+      />
+      <h2 className="text-lg font-medium">
+        Welcome to {user?.firstName}&apos;s Jotion
+      </h2>
+      <Button>
+        <PlusCircle className="h-4 w-4 mr-2" />
+        Create a note
+      </Button>
+    </div>
+  );
 };
 
 export default DocumentsPage;

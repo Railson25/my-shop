9090086d3a35f101421e57e19296d23971abbf6c
Railson25 <railsonsantiago123@gmail.com>
2023-11-18 12:34:01 -0300

    <style>: hover bg color

--- notion-clone/app/(main)/_components/trash-box.tsx
+++ notion-clone/app/(main)/_components/trash-box.tsx
@@ -91,7 +91,7 @@ export const TrashBox = () => {
               <div
                 onClick={(e) => onRestore(e, document._id)}
                 role="button"
-                className="rounded-sm p-2 hover:bg-neutral-200"
+                className="rounded-sm p-2 hover:bg-neutral-200 dark:hover:bg-neutral-600"
               >
                 <Undo className="h-4 w-4 text-muted-foreground" />
               </div>

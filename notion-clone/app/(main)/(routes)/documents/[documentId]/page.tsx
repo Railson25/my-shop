931e15c01969ab37ab08d0fbfd310df874bd843c
Railson25 <railsonsantiago123@gmail.com>
2023-11-22 15:15:28 -0300

    <feat>: on change funtion and dynamic import of editor

--- notion-clone/app/(main)/(routes)/documents/[documentId]/page.tsx
+++ notion-clone/app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,11 +1,16 @@
 "use client";
 
-import { Cover } from "@/components/cover";
-import { Toolbar } from "@/components/toolbar";
-import { Skeleton } from "@/components/ui/skeleton";
+import { useMutation, useQuery } from "convex/react";
+import dynamic from "next/dynamic";
+import { useMemo } from "react";
+
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
-import { useQuery } from "convex/react";
+
+import { Cover } from "@/components/cover";
+
+import { Toolbar } from "@/components/toolbar";
+import { Skeleton } from "@/components/ui/skeleton";
 
 interface DocumentIdPageProps {
   params: {
@@ -14,9 +19,23 @@ interface DocumentIdPageProps {
 }
 
 const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
+  const Editor = useMemo(
+    () => dynamic(() => import("@/components/editor"), { ssr: false }),
+    []
+  );
+
   const document = useQuery(api.documents.getById, {
     documentId: params.documentId,
   });
+
+  const update = useMutation(api.documents.update);
+
+  const onChange = (content: string) => {
+    update({
+      id: params.documentId,
+      content,
+    });
+  };
 
   if (document === undefined) {
     return (
@@ -43,6 +62,7 @@ const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
       <Cover url={document.coverImage} />
       <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
         <Toolbar initialData={document} />
+        <Editor onChange={onChange} initialContent={document.content} />
       </div>
     </div>
   );

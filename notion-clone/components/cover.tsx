1f23d0f80585172e219918d380cb771ae573b1c1
Railson25 <railsonsantiago123@gmail.com>
2023-11-22 11:04:28 -0300

    <feat>: cover skeleton function

--- notion-clone/components/cover.tsx
+++ notion-clone/components/cover.tsx
@@ -10,6 +10,7 @@ import { api } from "@/convex/_generated/api";
 import { useParams } from "next/navigation";
 import { Id } from "@/convex/_generated/dataModel";
 import { useEdgeStore } from "@/lib/edgestore";
+import { Skeleton } from "./ui/skeleton";
 
 interface CoverImagePros {
   url?: string;
@@ -64,4 +65,8 @@ export const Cover = ({ preview, url }: CoverImagePros) => {
       )}
     </div>
   );
+};
+
+Cover.Skeleton = function CoverSkeleton() {
+  return <Skeleton className="w-full h-[12vh]" />;
 };

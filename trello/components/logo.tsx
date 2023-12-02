96b942410ca3f8ca2d1a86990b2c7316579e6c34
Railson25 <railsonsantiago123@gmail.com>
2023-12-02 12:50:52 -0300

    <feat>: logo component

---
+++ trello/components/logo.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import localFont from "next/font/local";
+
+import { cn } from "@/lib/utils";
+
+const headingFont = localFont({
+  src: "../public/fonts/font.woff2",
+});
+
+export const Logo = () => {
+  return (
+    <Link href="/">
+      <div className="hover:opacity-75 transition justify-center items-center gap-x-2 hidden md:flex">
+        <Image alt="Logo" src="/logo.svg" height={300} width={30} />
+        <p
+          className={cn("text-lg text-neutral-700 pb-1", headingFont.className)}
+        >
+          Taskify
+        </p>
+      </div>
+    </Link>
+  );
+};

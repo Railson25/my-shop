7a9aeb00d2a6ba65656c55ee801ac999e4035efe
Railson25 <railsonsantiago123@gmail.com>
2023-12-08 15:50:03 -0300

    <refactor>: Dashboard layout

--- trello/app/(platform)/(dashboard)/organization/layout.tsx
+++ trello/app/(platform)/(dashboard)/organization/layout.tsx
@@ -1,12 +1,16 @@
-import { Navbar } from "../_components/navbar";
+import { Sidebar } from "../_components/sidebar";
 
-const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+const OrganizationLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="h-full">
-      <Navbar />
-      {children}
-    </div>
+    <main className="pt-20 md:pt-24 px-4 max-w-6xl 2xl:max-w-screen-xl mx-auto">
+      <div className="flex">
+        <div className="w-64 shrink-0 hidden md:block">
+          <Sidebar />
+        </div>
+        {children}
+      </div>
+    </main>
   );
 };
 
-export default DashboardLayout;
+export default OrganizationLayout;

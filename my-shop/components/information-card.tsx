49a91d2418c34b3bddf55282e86ee1af134c766a
Railson25 <railsonsantiago123@gmail.com>
2024-01-02 15:52:57 -0300

    <refactor>: Information card

--- my-shop/components/information-card.tsx
+++ my-shop/components/information-card.tsx
@@ -1,47 +1,46 @@
-"use client";
+import getBillboard from "@/actions/get-billboard";
 
 import { twMerge } from "tailwind-merge";
 
 interface InformationCardProps {
-  title?: string;
-  subtitle: string;
-  description?: string;
   className?: string;
   children?: React.ReactNode;
   small?: boolean;
   sell?: string;
+  billboardId: string;
 }
 
-export const InformationCard = ({
+export const InformationCard = async ({
   sell,
   small,
-  description,
-  subtitle,
-  title,
+
   className,
+  billboardId,
   children,
 }: InformationCardProps) => {
+  const billboard = await getBillboard(billboardId!);
+
   return (
     <div
       className={twMerge(
-        `group flex flex-col justify-center items-start  min-w-[580px] h-[50vh] bg-cover bg-no-repeat bg-center p-[30px] bg-[url('/images/banner/b17.jpg')]  max-md:min-w-full max-md:h-[30vh] 
-    
-        `,
+        `group flex flex-col justify-center items-start  min-w-[580px] h-[50vh] bg-cover  bg-no-repeat bg-center p-[30px] max-md:min-w-full max-md:h-[30vh] 
+      `,
         className
       )}
+      style={{ backgroundImage: `url(${billboard.imageUrl})` }}
     >
-      <h4 className="text-white text-[20px] font-light">{title}</h4>
+      <h4 className="text-white text-[20px] font-light">{billboard.label}</h4>
       <h2
         className={twMerge(`text-white text-[28px] 
                     ${small ? "font-black" : "font-extrabold"}
                     ${small ? "text-[22px]" : "text-[28px]"}
                 `)}
       >
-        {subtitle}
+        {billboard.subtitle}
       </h2>
       <h3 className="text-[#ec544e] font-extrabold text-[15px]">{sell}</h3>
       <span className="text-white text-[14px] font-medium pb-[15px]">
-        {description}
+        {billboard.descriptionSeason}
       </span>
       {children}
     </div>

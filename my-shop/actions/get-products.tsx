e7704b41bf4b50dc33833ab160a2b114bcc8e619
Railson25 <railsonsantiago123@gmail.com>
2024-01-02 15:50:49 -0300

    <feat>: getProducts action

---
+++ my-shop/actions/get-products.tsx
@@ -0,0 +1,28 @@
+import qs from "query-string";
+
+import { Product } from "@/types/types";
+
+const URL = `${process.env.NEXT_PUBLIC_API_URL}/products`;
+
+interface Query {
+  categoryId?: string;
+  colorId?: string;
+  sizeId?: string;
+  isFeatured?: boolean;
+}
+const getProducts = async (query: Query): Promise<Product[]> => {
+  const url = qs.stringifyUrl({
+    url: URL,
+    query: {
+      colorId: query.colorId,
+      categoryId: query.categoryId,
+      sizeId: query.sizeId,
+      isFeatured: query.isFeatured,
+    },
+  });
+  const res = await fetch(url);
+
+  return res.json();
+};
+
+export default getProducts;

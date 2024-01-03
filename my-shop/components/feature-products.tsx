e5830246f6c85ef6d6e52ca4842118a0cdd8901f
Railson25 <railsonsantiago123@gmail.com>
2024-01-03 14:41:03 -0300

    <refactor>: feature product

--- my-shop/components/feature-products.tsx
+++ my-shop/components/feature-products.tsx
@@ -4,19 +4,21 @@ import { Product } from "@/types/types";
 import { ProductCard } from "./feature-product-card";
 
 interface ProductsListProps {
-  title: string;
+  title?: string;
   items: Product[];
+  children?: React.ReactNode;
 }
 
-export const ProductsList = ({ title, items }: ProductsListProps) => {
+export const ProductsList = ({ title, items, children }: ProductsListProps) => {
   return (
     <div className="py-10 px-20 text-center bg-white max-[477px]:p-[20px]">
       <h1>{title}</h1>
 
       <div className="flex justify-between pt-5 flex-wrap max-md:justify-center max-md:gap-x-[15px]">
-        {items.map((item) => (
-          <ProductCard key={item.id} data={item} />
-        ))}
+        {children
+          ? null
+          : items.map((item) => <ProductCard key={item.id} data={item} />)}
+        {children}
       </div>
     </div>
   );

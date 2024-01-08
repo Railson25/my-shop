411844e4ae19b53dbf8b4f43c827e02e348678e5
Railson25 <railsonsantiago123@gmail.com>
2024-01-08 18:35:46 -0300

    <fix>: onClick function

--- my-shop/components/feature-products.tsx
+++ my-shop/components/feature-products.tsx
@@ -2,6 +2,7 @@
 
 import { Product } from "@/types/types";
 import { ProductCard } from "./feature-product-card";
+import { useRouter } from "next/navigation";
 
 interface ProductsListProps {
   title?: string;
@@ -10,6 +11,7 @@ interface ProductsListProps {
 }
 
 export const ProductsList = ({ title, items, children }: ProductsListProps) => {
+  const router = useRouter();
   return (
     <div className="py-10 px-20 text-center bg-white max-[477px]:p-[20px]">
       <h1>{title}</h1>
@@ -17,7 +19,13 @@ export const ProductsList = ({ title, items, children }: ProductsListProps) => {
       <div className="flex justify-between pt-5 flex-wrap max-md:justify-center max-md:gap-x-[15px]">
         {children
           ? null
-          : items.map((item) => <ProductCard key={item.id} data={item} />)}
+          : items.map((item) => (
+              <ProductCard
+                key={item.id}
+                data={item}
+                onclick={() => router.push(`/shop/product/${item.id}`)}
+              />
+            ))}
         {children}
       </div>
     </div>
